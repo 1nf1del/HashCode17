f0170d2bdd97fdfc58d9621dbb012d6f034a40c4
Jonas Vanden Branden <vandenbranden.jonas@gmail.com>
2017-02-23 19:50:31 +0100

    caches sorted

--- problem/EndPoint.py
+++ problem/EndPoint.py
@@ -1,8 +1,15 @@
+from Cache import Cache
+import operator
+
 class EndPoint:
     def __init__(self, datacenter_latency):
-        self.caches = dict()
+        self.caches = list()
         self.datacenter_latency = datacenter_latency
 
 
     def addCache(self,id,latency):
-        self.caches[id] = latency
+        cache = Cache(id)
+        self.caches.append((cache, latency))
+
+    def sortCaches(self):
+        self.caches.sort(key=operator.itemgetter(1))
--- problem/solution.py
+++ problem/solution.py
@@ -3,3 +3,4 @@ from InputParser import InputParser
 if __name__ == "__main__":
     parser = InputParser()
     parser.parse("in/me_at_the_zoo.in")
+
