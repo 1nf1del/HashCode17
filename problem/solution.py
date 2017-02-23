e9afcbf6f5b1f127072c9ae278bb38bf4c0448cf
Jonas Vanden Branden <vandenbranden.jonas@gmail.com>
2017-02-23 20:17:35 +0100

    bla

--- problem/solution.py
+++ problem/solution.py
@@ -1,14 +1,24 @@
 from InputParser import InputParser
 from CacheManager import CacheManager
 
-if __name__ == "__main__":
+def process_file(filename):
     parser = InputParser()
-    parser.parse("in/me_at_the_zoo.in")
+    parser.parse("in/" + filename + ".in")
     manager = CacheManager()
     manager.videos = parser.getVideos()
     manager.caches = parser.getCaches()
     manager.endpoints = parser.getEndPoints()
     manager.requests = parser.getRequests()
 
-
     manager.FillCaches()
+
+    outputstring = manager.OutputString()
+    outputfile = open("out/" + filename + ".out", 'w')
+    outputfile.write(outputstring)
+
+if __name__ == "__main__":
+    files = ["me_at_the_zoo", "kittens", "trending_today", "videos_worth_spreading"]
+
+    for file in files[0:1]:
+        process_file(file)
+
