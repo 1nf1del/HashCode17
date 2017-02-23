179a357fcf5394a31fd1b05f0e2f6bf5f40d1e0c
Jonas Vanden Branden <vandenbranden.jonas@gmail.com>
2017-02-23 19:25:16 +0100

    more

--- Practice/InputParser.py
+++ Practice/InputParser.py
@@ -1,34 +1,47 @@
 from Instances/Video import Video
+from Instances/EndPoint import EndPoint
+from Instances/Cache import Cache
+from Instances/Request import Request
 
 class InputParser:
     def __init__(self):
-        self.videolist = list()
+        self.videoList = list()
+        self.cacheList = list()
 
     def parse(self,filename):
         inputfile = open(filename, 'r')
         firstline = inputfile.readline()
         print(firstline)
         param = firstline[:-1].split(' ')
-        num_videos, num_endpoints, num_descriptions, num_caches, num_capacity = int(param[0]), int(param[1]), int(param[2]), int(param[3]), int(param[4])
+        num_videos, num_endpoints, num_descriptions, num_caches, capacity = int(param[0]), int(param[1]), int(param[2]), int(param[3]), int(param[4])
         videoline = inputfile.readline()
         videos = videoline[:-1].split(' ')
 
         for size in videos:
             vid = Video(size)
-            self.videolist.append(vid)
+            self.videoList.append(vid)
 
-        for
+        for i in range(num_caches):
+            cache = Cache(capacity)
+            self.cacheList.append(cache)
+
+
 
         for endpoint in num_endpoints:
             ep_line = inputfile.readline()[:-1]
             ep_line = ep_line.split(' ')
             datacenter_latency = ep_line[0]
             cache_count = ep_line[1]
+            ep = EndPoint(datacenter_latency)
             for cache in cache_count:
                 cache_line = inputfile.readline()[:-1]
                 cache_line = cache_line.split(' ')
                 cache_id = cache_line[0]
                 cache_latency = cache_line[1]
+                ep.addCache(cache_id, cache_latency)
+
+
+        for i in range(num_descriptions):
 
 
 
--- problem/Instances/EndPoint.py
+++ problem/Instances/EndPoint.py
@@ -1,3 +1,8 @@
 class EndPoint:
-    def __init__(self):
+    def __init__(self, datacenter_latency):
         self.caches = dict()
+        self.datacenter_latency = datacenter_latency
+
+
+    def addCache(self,id,latency):
+        self.caches[id] = latency
