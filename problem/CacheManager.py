9b48bb79b61ac335120c4f52e665d147334921e0
Robin <robin.depauw5@gmail.com>
2017-02-23 20:09:09 +0100

    print statement

--- problem/CacheManager.py
+++ problem/CacheManager.py
@@ -6,14 +6,12 @@ class CacheManager:
         self.requests = list()
 
     def FillCaches(self):
-        self.requests.sort(key=lambda req:req.number, reverse=True)
+        self.requests.sort(key=lambda req: req.number, reverse=True)
         for request in self.requests:
             endpoint = request.endpoint
             video = request.video
-            for (cache,latency) in endpoint.caches:
+            print("Requests:{0!s} video:{0!s} EP:{0!s}".format(request.number, video.id, endpoint.id))
+            for (cache, latency) in endpoint.caches:
                 if cache.put(video):
-                    print("")
+                    print("Put in cache:{0!s} latency:{0!s}ms".format(cache.id, latency))
                     break
-
-
-
